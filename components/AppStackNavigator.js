fac69d53c4b7567768d6b3d1e69e96681c38298b
zahirahmed <zahir.eleganzit@gmail.com>
2019-07-11 15:54:34 +0530

    zahir 11-07-2019

---
+++ components/AppStackNavigator.js
@@ -0,0 +1,20 @@
+// import Splash from './Splash';
+// import Login from './Login';
+
+// import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+// const AppNavigator = createStackNavigator({
+//     //Screens
+//     Splash: {
+//         screen : Splash
+//     },
+//     Login: {
+//         screen: Login
+//     },  
+
+// }, {
+//     //Settings
+//     initialRouteName: 'Splash'
+// })
+
+// export default createAppContainer(AppNavigator)
